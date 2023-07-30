c14dc9e3f376c84102681f0273dc5df9ebea0e2b
Sanghak Lee <yee950419@naver.com>
2023-07-30 14:08:58 +0900

    Initial commit

---
+++ src/main/java/com/springboot/webapplicationpractice/WebapplicationpracticeApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.webapplicationpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebapplicationpracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebapplicationpracticeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springboot/webapplicationpractice/WebapplicationpracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.webapplicationpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebapplicationpracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
